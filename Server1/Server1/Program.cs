87a101172ae1ac645ae6d572021a91fc5c3b69f3
yoolcool <redman84m@naver.com>
2015-12-04 10:32:55 +0900

    기본형태

--- Server1/Server1/Program.cs
+++ Server1/Server1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Server1
 {
@@ -10,6 +12,29 @@ namespace Server1
     {
         static void Main(string[] args)
         {
+            Thread serverThread = new Thread(serverFunc);
+            serverThread.IsBackground = true;
+            serverThread.Start();
+
+            Thread.Sleep(500);
+
+            Thread clientThread = new Thread(clientFunc);
+            clientThread.IsBackground = true;
+            clientThread.Start();
+
+            Console.WriteLine("종료하려면 아무 키나 누르세요.");
+            Console.ReadLine();
+
+        }
+
+        private static void serverFunc(object obj)
+        {
+
+        }
+
+        private static void clientFunc(object obj)
+        {
+
         }
     }
 }
