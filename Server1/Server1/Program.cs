994b1ccbec6b992cbc7d25645a35547ff39d6377
yoolcool <redman84m@naver.com>
2015-12-04 10:49:21 +0900

    TCP 가장 기본

--- Server1/Server1/Program.cs
+++ Server1/Server1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 
@@ -29,11 +30,50 @@ namespace Server1
 
         private static void serverFunc(object obj)
         {
+            using (Socket srvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 11200);
+                srvSocket.Bind(endPoint);
+                srvSocket.Listen(10);
 
+                while (true)
+                {
+                    Socket clntSocket = srvSocket.Accept();
+
+                    byte[] recvBytes = new byte[1024];
+
+                    int nRecv = clntSocket.Receive(recvBytes);
+                    string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv);
+
+                    byte[] sendBytes = Encoding.UTF8.GetBytes("Hello: " + txt);
+                    clntSocket.Send(sendBytes);
+                    clntSocket.Close();
+                }
+
+            }
         }
 
         private static void clientFunc(object obj)
         {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                EndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 11200);
+
+                socket.Connect(serverEP);
+
+                byte[] buf = Encoding.UTF8.GetBytes(DateTime.Now.ToString());
+                socket.Send(buf);
+
+                byte[] recvBytes = new byte[1024];
+                int nRecv = socket.Receive(recvBytes);
+                string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv);
+
+                Console.WriteLine(txt);
+
+                socket.Close();
+                Console.WriteLine("TCP CLiendt socket: Closed");
+
+            }
 
         }
     }
